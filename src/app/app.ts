f42c4f00657ca6023a8eacc025ffaadb46df4da4
natansalvadorligabo <salvadorligabo@gmail.com>
2025-06-06 21:20:25 -0300

    feat: add sidenav and main content layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSidenavModule, MatIconModule, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
