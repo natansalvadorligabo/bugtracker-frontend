751b6dc61f5aa0dacbe7ff0542a37f3ec727dcf1
Igor Cardoso <igorfilipicardoso@gmail.com>
2025-06-18 22:00:47 -0300

    feat: implement login functionality with form validation and error handling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,17 @@ export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'register' },
   {
     path: 'register',
-    loadComponent: () => import('./container/register/register')
-      .then(m => m.Register)
+    loadComponent: () =>
+      import('./container/register/register').then((m) => m.Register),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./container/login/login').then((m) => m.Login),
   },
   {
     path: 'tickets',
-    loadComponent: () => import('./container/tickets/tickets')
-      .then(m => m.Tickets)
+    loadComponent: () =>
+      import('./container/tickets/tickets').then((m) => m.Tickets),
   },
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: '' },
 ];
