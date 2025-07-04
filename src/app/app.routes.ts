e5f67f2d4931fe905c66a5c91977b553818a17ec
natansalvadorligabo <salvadorligabo@gmail.com>
2025-07-04 20:16:25 -0300

    feat: restructure routing and implement layout component with responsive design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,19 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'register' },
+  {
+    path: '',
+    loadComponent: () => import('./shared/layout/layout').then((m) => m.Layout),
+    children: [
+      { path: '', redirectTo: 'tickets', pathMatch: 'full' },
+      {
+        path: 'tickets',
+        loadComponent: () =>
+          import('./container/tickets/tickets').then((m) => m.Tickets),
+      },
+    ],
+  },
+
   {
     path: 'register',
     loadComponent: () =>
@@ -11,10 +23,6 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () => import('./container/login/login').then((m) => m.Login),
   },
-  {
-    path: 'tickets',
-    loadComponent: () =>
-      import('./container/tickets/tickets').then((m) => m.Tickets),
-  },
+
   { path: '**', redirectTo: '' },
 ];
